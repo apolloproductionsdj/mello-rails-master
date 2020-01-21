53be9ec5aff666708db3aa02989d111f66c24da1
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-21 04:36:50 +0000

    started the boards, redirected users from the client using location.replace, manipulated local storage from the Chrome DevTools

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -7,6 +7,14 @@ const $message = $('#message');
 
 let authSetting = 'login';
 
+sendUserToBoards();
+
+function sendUserToBoards() {
+  if (localStorage.getItem('user')) {
+    location.replace('/boards');
+  }
+}
+
 function setAuth(setting) {
   authSetting = setting;
 
@@ -58,9 +66,9 @@ function handleLoginResponse(data, status, jqXHR) {
   if (status === 'success') {
     let jwt = jqXHR.getResponseHeader('authorization');
     let user = JSON.stringify(data);
-
     localStorage.setItem('authorization', jwt);
     localStorage.setItem('user', user);
+    sendUserToBoards();
   } else {
     displayMessage('Invalid email or password.', 'danger');
   }
