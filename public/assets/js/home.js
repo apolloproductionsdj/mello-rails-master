9bc8e30ef84b07a0c0831a65936a9437f90f2ac8
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-21 00:52:21 +0000

    set input values using jQuery, set element attributes using jQuery, used Postman to utest API endpoints and used the jQuery $.ajax method to make AJAX requests

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,12 +1,15 @@
+const $submitButton = $('#submit');
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
-const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
 function setAuth(setting) {
   authSetting = setting;
-  
+
   if (authSetting === 'login') {
     $setLogin.addClass('active');
     $setSignUp.removeClass('active');
@@ -18,5 +21,42 @@ function setAuth(setting) {
   }
 }
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger');
+    return;
+  }
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting,
+    data: {
+      user: {
+        email: email,
+        password: password
+      }
+    },
+    method: 'POST'
+  }).then(function(data) {
+    console.log(data);
+  });
+}
+
 $setLogin.on('click', setAuth.bind(null, 'login'));
 $setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
