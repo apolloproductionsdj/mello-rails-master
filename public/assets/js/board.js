424ae54620cf9e71a30804ee60130b534ca5389f
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-23 03:05:00 +0000

    added ability add contributors and have them listed

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -11,6 +11,11 @@ const $editListDeleteButton = $('#edit-list .delete');
 const $editCardInput = $('#edit-card textarea');
 const $editCardSaveButton = $('#edit-card .save');
 const $editCardDeleteButton = $('#edit-card .delete');
+const $contributorModalButton = $('#contributors');
+const $contributorModalInput = $('#contributor-email');
+const $contributorModalSaveButton = $('#contribute .save');
+const $contributorModalList = $('#contributors-content ul');
+
 
 let board;
 
@@ -45,14 +50,14 @@ function handleLogout() {
   });
 }
                                                         
-function createCards(cards) {
+function createCards(list) {
   let $cardUl = $('<ul>');
 
-  let $cardLis = cards.map(function(card) {
+  let $cardLis = list.cards.map(function(card) {
     let $cardLi = $('<li>');
     let $cardButton = $('<button>')
       .text(card.text)
-      .data(card)
+      .data({ ...card, list_id: list.id })
       .on('click', openCardEditModal);
 
     $cardLi.append($cardButton);
@@ -67,24 +72,24 @@ function createCards(cards) {
 
 function createLists(lists) {
   let $listContainers = lists.map(function(list) {
-    let $listContainer = $('<div class="list">').data('id', list.id);
-    let $header = $('<header>');
-    let $headerButton = $('<button>').text(list.title)
-    .data(list)
-    .on('click', openListEditModal);
-    let $cardUl = createCards(list.cards);
-    let $addCardButton = $('<button>Add a card...</button>').on(
-      'click',
-      openCardCreateModal
-    );
+  let $listContainer = $('<div class="list">').data(list);
+  let $header = $('<header>');
+  let $headerButton = $('<button>').text(list.title)
+  .data(list)
+  .on('click', openListEditModal);
+  let $cardUl = createCards(list);
+  let $addCardButton = $('<button>Add a card...</button>').on(
+    'click',
+    openCardCreateModal
+  );
 
-    $header.append($headerButton);
-    $listContainer.append($header);
-    $listContainer.append($cardUl);
-    $listContainer.append($addCardButton);
+  $header.append($headerButton);
+  $listContainer.append($header);
+  $listContainer.append($cardUl);
+  $listContainer.append($addCardButton);
 
-    return $listContainer;
-  });
+  return $listContainer;
+});
 
   let $addListContainer = $('<div class="list add">');
   let $addListButton = $('<button>')
@@ -101,14 +106,92 @@ function openListCreateModal() {
   $createListInput.val('');
   MicroModal.show('create-list');
 }
-  function renderBoard() {
-    let $lists = createLists(board.lists);
+  
+function renderBoard() {
+  let $lists = createLists(board.lists);
 
-    $boardName.text(board.name);
+  $boardName.text(board.name);
 
-    $boardContainer.empty();
-    $boardContainer.append($lists);
-  }
+  $boardContainer.empty();
+  $boardContainer.append($lists);
+
+  makeSortable();
+  renderContributors();
+}
+
+function renderContributors() {
+  let $contributorListItems = board.users.map(function(user) {
+    let $contributorListItem = $('<li>').text(user.email);
+    return $contributorListItem;
+  });
+
+  $contributorModalList.empty();
+  $contributorModalList.append($contributorListItems);
+}
+
+function makeSortable() {
+  Sortable.create($boardContainer[0], {
+    animation: 150,
+    ghostClass: 'ghost',
+    filter: '.add',
+    easing: 'cubic-bezier(0.785, 0.135, 0.15, 0.86)',
+    onMove: function(event) {
+      let shouldMove = !$(event.related).hasClass('add');
+      return shouldMove;
+    },
+    onEnd: function(event) {
+      let { id, position } = $(event.item).data();
+      let newPosition = event.newIndex + 1;
+
+      if (position === newPosition) {
+        return;
+      }
+
+      $.ajax({
+        url: `/api/lists/${id}`,
+        data: {
+          position: newPosition
+        },
+        method: 'PUT'
+      }).then(function() {
+        init();
+      });
+    }
+  });
+
+ $('.list > ul').each(function(index, element) {
+  Sortable.create(element, {
+    animation: 150,
+    ghostClass: 'ghost',
+    easing: 'cubic-bezier(0.785, 0.135, 0.15, 0.86)',
+    group: 'shared',
+    onEnd: function(event) {
+      let { id, position, list_id } = $(event.item)
+        .find('button')
+        .data();
+      let newPosition = event.newIndex + 1;
+      let newListId = $(event.item)
+        .parents('.list')
+        .data('id');
+
+      if (position === newPosition && list_id === newListId) {
+        return;
+      }
+
+      $.ajax({
+        url: `/api/cards/${id}`,
+        method: 'PUT',
+        data: {
+          list_id: newListId,
+          position: newPosition
+        }
+      }).then(function() {
+        init();
+      });
+    }
+  });
+});
+}
 
 function handleListCreate(event) {
   event.preventDefault();
@@ -263,7 +346,70 @@ function handleCardDelete(event) {
   });
 }
 
+function displayMessage(msg, type = 'hidden') {
+  $('#contribute .message')
+    .attr('class', `message ${type}`)
+    .text(msg);
+}
 
+function handleContributorSave(event) {
+  event.preventDefault();
+
+  let emailRegex = /.+@.+\..+/;
+
+  let contributorEmail = $contributorModalInput.val().trim();
+  
+  $contributorModalInput.val('');
+
+  if (!emailRegex.test(contributorEmail)) {
+    displayMessage('Must provide a valid email address', 'danger');
+    return;
+  }
+
+  let contributor = board.users.find(function(user) {
+    return user.email === contributorEmail;
+  });
+
+  if (contributor) {
+    displayMessage(
+    `${contributorEmail} already has access to the board`,
+    'danger'
+    );
+    return;
+  }
+
+  $.ajax({
+    url: '/api/user_boards',
+    method: 'POST',
+    data: {
+      email: contributorEmail,
+      board_id: board.id
+    }
+  })
+    .then(function() {
+      init();
+      displayMessage(
+        `Successfully added ${contributorEmail} to the board`,
+        'success'
+      );
+    })
+    .catch(function() {
+      displayMessage(
+        `Cannot find user with email: ${contributorEmail}`,
+        'danger'
+      );
+    });
+}
+
+function openContributorModal() {
+  $contributorModalInput.val('');
+  displayMessage('');
+
+  MicroModal.show('contribute');
+}
+
+$contributorModalSaveButton.on('click', handleContributorSave);
+$contributorModalButton.on('click', openContributorModal);
 $saveCardButton.on('click', handleCardCreate);
 $saveListButton.on('click', handleListCreate);
 $logoutButton.on('click', handleLogout);
